5eb0ac1cd59d1cc5c89fd9e19a52f1b5cf267023
Aidan Lane <aidanlane0@protonmail.com>
2022-11-24 14:26:58 -0500

    saving original data

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ class App extends Component {
 
     this.state = {
       monsters: [], // initiliaze as empty for empty case
+      searchField: ''
     };
+    console.log('constructor');
   }
 
   // lifecycle method
   // Mounting is the first time a component gets placed on the doc
   // might remount if it has been unmounted. Basically new instance of it
   componentDidMount() {
+    console.log('componentdidmount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json()) // if response
       .then((users) => this.setState(() => {
@@ -28,10 +31,25 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          console.log(event.target.value);
+          const searchField = event.target.value.toLowerCase();
+          // if you want to modify an array, you want to generate a new one with the modification
+
+          this.setState(() => {
+            return { searchField }; // Key and value are 'searchField'
+          });
+        }}/>
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return(
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
